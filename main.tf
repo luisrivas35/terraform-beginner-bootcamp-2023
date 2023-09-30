7ec2c892047031e3e8cc6bb329c7fd2c9e0d8b56
Luis Rivas <xluisrivas@gmail.com>
2023-09-30 17:15:05 +0000

    enabling website hostingupload error and index

--- main.tf
+++ main.tf
@@ -4,7 +4,11 @@ terraform{
 
 module "terrahouse_aws" {
     source ="./modules/terrahouse_aws"
-    user_uuid ="dacbd37c-4b14-45b0-b5f0-13f0eacd8c16"
-    bucket_name ="56q40qa2vq8847w5"
+    # user_uuid ="dacbd37c-4b14-45b0-b5f0-13f0eacd8c16"
+    # bucket_name ="56q40qa2vq8847w5"
+    user_uuid = var.user_uuid
+    bucket_name = var.bucket_name
+    index_html_filepath = var.index_html_filepath
+    error_html_filepath = var.error_html_filepath
 }
 
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "bucket_name" {
   description = "bucket for static web site"
   value = module.terrahouse_aws.bucket_name
 }
+
+output "s3_website_endpoint" {
+  value = module.terrahouse_aws.website_endpoint
+}
