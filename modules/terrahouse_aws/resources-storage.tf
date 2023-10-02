e85410afd6fb3ee6168ae05b25689969d79e4696
Luis Rivas <xluisrivas@gmail.com>
2023-10-02 19:11:20 -0300

    invalidate cloufront stack when content version changes (#29)

--- modules/terrahouse_aws/resources-storage.tf
+++ modules/terrahouse_aws/resources-storage.tf
@@ -52,29 +52,30 @@ resource "aws_s3_object" "error_html" {
   }
 }
 
+# 
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.website_bucket.bucket
-  #policy = data.aws_iam_policy_document.allow_access_from_another_account.json
   policy = jsonencode({
-    "Version"= "2012-10-17",
-      "Statement"= {
-         "Sid"= "AllowCloudFrontServicePrincipalReadOnly",
-          "Effect"= "Allow",
-          "Principal"= {
-             "Service"= "cloudfront.amazonaws.com"
-          },
-      "Action"= "s3:GetObject",
-      "Resource"= "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*",
-      "Condition"= {
-         "StringEquals"= {
-            "AWS:SourceArn"= "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
-            #"AWS:SourceArn": data.aws_caller_identity.current.arn
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontServicePrincipalReadOnly",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        },
+        Action    = "s3:GetObject",
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.website_bucket.bucket}/*",
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
+          }
         }
       }
-    }
+    ]
   })
-
 }
+
 
 resource "terraform_data" "content_version" {
   input = var.content_version
