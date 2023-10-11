574190629794bef391d0286d9d0b50fb35d2f901
Luis Rivas <xluisrivas@gmail.com>
2023-10-11 15:58:05 -0300

    36 terrratowns provider (#38)
    * working progress setting up skeleton creating tf provider
    
    * #36 implement tf provider block in terraforrm

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
-output "bucket_name" {
-  description = "bucket for static web site"
-  value = module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description = "bucket for static web site"
+#   value = module.terrahouse_aws.bucket_name
+# }
 
-output "s3_website_endpoint" {
-  value = module.terrahouse_aws.website_endpoint
-}
+# output "s3_website_endpoint" {
+#   value = module.terrahouse_aws.website_endpoint
+# }
 
-output "cloudfront_url" {
-  description = "the cloudfront distribution domain name"
-  value = module.terrahouse_aws.cloudfront_url
-}
+# output "cloudfront_url" {
+#   description = "the cloudfront distribution domain name"
+#   value = module.terrahouse_aws.cloudfront_url
+# }
