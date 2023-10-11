ce04d34fd8a9cc7c96505b31f0e163fc02c04185
Luis Rivas <xluisrivas@gmail.com>
2023-10-11 18:44:07 +0000

    #36 implement tf provider block in terraforrm

--- main.tf
+++ main.tf
@@ -1,16 +1,29 @@
 terraform{
-
+    required_providers {
+        terratowns = {
+            source = "local.providers/local/terratowns"
+            version = "1.0.0"
+        }
+      
+    }   
 }
 
-module "terrahouse_aws" {
-    source ="./modules/terrahouse_aws"
-    # user_uuid ="dacbd37c-4b14-45b0-b5f0-13f0eacd8c16"
-    # bucket_name ="56q40qa2vq8847w5"
-    user_uuid = var.user_uuid
-    bucket_name = var.bucket_name
-    index_html_filepath = var.index_html_filepath
-    error_html_filepath = var.error_html_filepath
-    content_version = var.content_version
-    assets_path = var.assets_path
+provider "terratowns" {
+    endpoint = "http://localhost:4567"
+    user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+    token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  
 }
+
+# module "terrahouse_aws" {
+#     source ="./modules/terrahouse_aws"
+#     # user_uuid ="dacbd37c-4b14-45b0-b5f0-13f0eacd8c16"
+#     # bucket_name ="56q40qa2vq8847w5"
+#     user_uuid = var.user_uuid
+#     bucket_name = var.bucket_name
+#     index_html_filepath = var.index_html_filepath
+#     error_html_filepath = var.error_html_filepath
+#     content_version = var.content_version
+#     assets_path = var.assets_path
+# }
 
